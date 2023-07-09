900533403a71dbb33b5fc86278c11b785062e327
Dallas Williamson <dallas.v.williamson@gmail.com>
2023-07-09 14:33:18 -0600

    snake-4: The Apple Update (#5)

--- snake/sounds.lua
+++ snake/sounds.lua
@@ -1,7 +1,8 @@
 local sounds = {
     ['music'] = love.audio.newSource('sounds/music.wav', 'static'),
     ['collect_item'] = love.audio.newSource('sounds/collect_item.wav', 'static'),
-    ['countdown'] = love.audio.newSource('sounds/countdown.wav', 'static')
+    ['countdown'] = love.audio.newSource('sounds/countdown.wav', 'static'),
+    ['game_over'] = love.audio.newSource('sounds/game_over.wav', 'static')
 }
 
 return sounds
--- snake/states.lua
+++ snake/states.lua
@@ -3,5 +3,6 @@ local states = {}
 states.START = 'start'
 states.PENDING_PLAY = 'pending_play'
 states.PLAY = 'play'
+states.GAME_OVER = 'game_over'
 
 return states
