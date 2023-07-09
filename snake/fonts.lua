69cd7a4fa35dfe961ad59301612959d04425e44a
Dallas Williamson <dallas.v.williamson@gmail.com>
2023-07-08 20:23:24 -0600

    Put font aliasing logic with fonts

--- snake/fonts.lua
+++ snake/fonts.lua
@@ -1,20 +1,23 @@
-local fonts = {
-    ['small'] = {
-        ['size'] = 8,
-        ['half_size'] = 4,
-        ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 8)
+local fonts = {}
 
-    },
-    ['medium'] = {
-        ['size'] = 16,
-        ['half_size'] = 8,
-        ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 16)
-    },
-    ['large'] = {
-        ['size'] = 32,
-        ['half_size'] = 16,
-        ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 32)
-    }
+love.graphics.setDefaultFilter('nearest', 'nearest') -- Make the aliasing pixely
+
+fonts.small = {
+    ['size'] = 8,
+    ['half_size'] = 4,
+    ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 8)
+}
+
+fonts.medium = {
+    ['size'] = 16,
+    ['half_size'] = 8,
+    ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 16)
+}
+
+fonts.large = {
+    ['size'] = 32,
+    ['half_size'] = 16,
+    ['font'] = love.graphics.newFont('fonts/Press_Start_2P/font.ttf', 32)
 }
 
 return fonts
--- snake/sounds.lua
+++ snake/sounds.lua
@@ -2,7 +2,4 @@ local sounds = {
     ['music'] = love.audio.newSource('sounds/music.wav', 'static')
 }
 
-love.graphics.setDefaultFilter('nearest', 'nearest') -- Make the aliasing pixely
-
-
 return sounds
